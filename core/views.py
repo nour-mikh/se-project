01a00f1b4e06cf2f22d168e500eebd0ffdd6c5b8
eliejoefarah <eliejoefarah@gmail.com>
2024-05-01 23:49:43 +0300

    help please

--- core/views.py
+++ core/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+def my_view(request):
+    menu_items = [
+        {'name': 'Button 1', 'url': '#'},
+        {'name': 'Button 2', 'url': '#'},
+        # ...
+    ]
+    return render(request, 'base.html', {'menu_items': menu_items})
