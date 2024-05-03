72f0b5eb8b8f91678283b0c6065caa160f7a2c94
eliejoefarah <eliejoefarah@gmail.com>
2024-05-03 08:54:53 +0300

    fixed links

--- project/urls.py
+++ project/urls.py
@@ -23,7 +23,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.main),
+    path('', views.main,name='index'),
     path('create-account/', views.create, name='create-account'),
     path('my-profile/',views.profile, name='my-profile'),
     path('bookings/',views.bookings, name='bookings'),
